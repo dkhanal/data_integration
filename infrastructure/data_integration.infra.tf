f162b81ce60d2945751595d5aa72b6550488e8e9
Deepak Khanal <dkhanal@gmail.com>
2018-02-28 13:43:21 -0600

    Incremental POC

--- infrastructure/data_integration.infra.tf
+++ infrastructure/data_integration.infra.tf
@@ -1,3 +1,23 @@
+variable "client_platform" {
+  default = "windows"
+}
+
+variable "user_dir" {
+  type = "map"
+  default = {
+    "windows" = "%USERPROFILE%"
+    "linux" = "$HOME"
+  }
+}
+
+variable "ssh_profile" {
+  type = "map"
+  default = {
+    "username" = "deepak"
+    "private_key" = "${var.user_dir[var.client_platform]}/DeepakKhanalAWSKey.pem"
+  }  
+}
+
 variable "region" {
   default                 = "us-west-2"
 }
@@ -10,4 +30,16 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami                   = "ami-6e1a0117"
   instance_type         = "t2.micro"
+
+  connection {
+    user = "ubuntu"
+    private_key = "${var.ssh_profile[var.client_platform]}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "git clone https://github.com/dkhanal/data_integration.git ~/data_integration",
+      "~/data_integration/infrastructure/scripts/installer-docker.sh"
+    ]
+  }  
 }
