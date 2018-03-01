59f37b4926449a316252884d5728f8578e350884
Deepak Khanal <dkhanal@gmail.com>
2018-02-28 21:12:26 -0600

    Updates

--- infrastructure/scripts/install_kafka.sh
+++ infrastructure/scripts/install_kafka.sh
@@ -4,9 +4,9 @@ sudo apt-get install oracle-java8-installer -y
 sudo java -version
 sudo apt-get install zookeeperd -y
 netstat -ant | grep :2181
-wget http://mirror.dsrg.utoronto.ca/apache/kafka/1.0.0/kafka-1.0.0-src.tgz
+wget http://apache.mirror.colo-serv.net/kafka/1.0.0/kafka_2.11-1.0.0.tgz
 sudo mkdir /opt/Kafka
 cd /opt/Kafka
-sudo tar -xvf kafka_2.10-0.10.0.1.tgz -C /opt/Kafka/
-sudo nohup /opt/Kafka/kafka_2.10-0.10.0.1/bin/kafka-server-start.sh /opt/Kafka/kafka_2.10-0.10.0.1/config/server.properties /tmp/kafka.log 2>&1 &
-sudo /opt/Kafka/kafka_2.10-0.10.0.1/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1  --partitions 1 --topic testing
+sudo tar -xvf kafka_2.11-1.0.0.tgz -C /opt/Kafka/
+sudo nohup /opt/Kafka/kafka_2.11-1.0.0/bin/kafka-server-start.sh /opt/Kafka/kafka_2.11-1.0.0/config/server.properties /tmp/kafka.log 2>&1 &
+sudo /opt/Kafka/kafka_2.11-1.0.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1  --partitions 1 --topic testing
